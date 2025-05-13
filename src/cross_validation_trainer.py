15c460ea0b8aaa785fc835c0cc55ed955b463e8d
Xinyue Fan <forlafan399@gmail.com>
2025-05-13 12:50:37 -0600

    store result in dictionary, move plot part to main.py

--- src/cross_validation_trainer.py
+++ src/cross_validation_trainer.py
@@ -1,4 +1,3 @@
-
 from typing import Tuple
 import numpy as np
 import matplotlib.pyplot as plt
@@ -9,7 +8,9 @@ from sklearn.model_selection import KFold
 from model import TradeHorizonScanModel
 from torcheval.metrics import R2Score
 from typing import List
+import numpy as np
 
+global all_the_results
 def cross_validate(
     dataset,
     hs_map_size: int,
@@ -18,27 +19,27 @@ def cross_validate(
     dim_exp: int,
     dim_imp: int,
     dim_cty: int,
-    k_splits: int = 5,
-    batch_size: int = 2048, #1024
+    k_splits: int = 5 ,
+    batch_size: int = 2048, 
     lr: float = 1e-3,
-    epochs: int = 200,
+    epochs: int = 5,#200,
     device: torch.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-) -> Tuple[float, float, float, float, List[np.ndarray]]:
-    #-> Tuple[float, float, float, float]:
+): 
+    global all_the_results
     kfold = KFold(n_splits=k_splits, shuffle=True, random_state=42)
     fold_losses = []
     fold_r2s = []
     all_fold_y = []  # store all y values for each fold
     all_fold_preds = [] # store all predictions for each fold
     all_fold_apes = []
-
-
+    all_the_results = {} #global dictionary to store the results
 
     for fold, (train_idx, val_idx) in enumerate(kfold.split(dataset), 1): #_
         print(f"K-Fold {fold} starts")
         train_loader = DataLoader(Subset(dataset, train_idx), batch_size=batch_size, shuffle=True, num_workers=4)
         val_loader = DataLoader(Subset(dataset, val_idx), batch_size=batch_size, shuffle=False, num_workers=4)
-
+        
+        #model set up
         model = TradeHorizonScanModel(
             n_hs = hs_map_size,
             #n_yr = yr_map_size,
@@ -50,6 +51,7 @@ def cross_validate(
         criterion = nn.MSELoss()
         optimizer = torch.optim.Adam(model.parameters(), lr=lr)
 
+        #train the model
         for _ in range(1, epochs + 1):
             print(f"Epoch {_} starts")
             model.train()
@@ -87,8 +89,6 @@ def cross_validate(
 
                 fold_y.append(y.cpu().numpy())  # colect y values for each fold
                 fold_preds.append(preds.cpu().numpy()) #collect preds for each fold
-
-
         val_loss /= len(val_idx)
         fold_losses.append(val_loss)
         #fold_r2s.append(r2_metric.compute())
@@ -108,54 +108,16 @@ def cross_validate(
                 print(f"Warning: Fold {fold} has no valid actual values (> 0), skipping MAPE calculation.")
                 fold_apes = np.array([])
                 mape = np.nan
-                all_fold_apes.append(fold_apes)
+                all_fold_apes.append(fold_apes)    
     
-
-    #plot
-    plt.figure(figsize=(12, 5))
-    plt.subplot(1, 2, 1)
-    folds = list(range(1, k_splits + 1))
-    plt.plot(folds, fold_losses, marker='o', linestyle='-', color='b', label='Validation Loss')
-    plt.title('Validation Loss (MSE) Across Folds')
-    plt.xlabel('Fold')
-    plt.ylabel('Mean Squared Error (MSE)')
-    plt.grid(True)
-    plt.legend()
-    
-    # Print R² Score
-    plt.subplot(1, 2, 2)
-    plt.plot(folds, fold_r2s, marker='o', linestyle='-', color='g', label='R² Score')
-    plt.title('R² Score Across Folds')
-    plt.xlabel('Fold')
-    plt.ylabel('R² Score')
-    plt.grid(True)
-    plt.legend()
-    
-    plt.tight_layout()
-    plt.savefig('mse_r2_plot.png')   
-    plt.show() 
-    #plot all y and preds for each fold separately
-
+    #set up variables
     all_y = np.concatenate(all_fold_y, axis=0)
     all_preds = np.concatenate(all_fold_preds, axis=0)
-    plt.figure(figsize=(6, 5))
-    plt.scatter(all_y, all_preds, alpha=0.5, s=10, label='Predicted vs True')
-    plt.plot([min(all_y), max(all_y)], [min(all_y), max(all_y)], 'r--', lw=2, label='Ideal')
-    plt.title('True vs Predicted Values (All Folds Combined)')
-    plt.xlabel('True Values')
-    plt.ylabel('Predicted Values')
-    plt.grid(True)
-    plt.legend()
-
-    plt.tight_layout()
-    plt.savefig('true_vs_pred_plot.png')
-    
-    plt.show() 
-    plt.close()   
-
-
- 
-    fold_mapes = []
+    mean_mse = float(np.mean(fold_losses)) #mean_loss
+    std_mse  = float(np.std(fold_losses)) #std_loss
+    mean_r2 = float(np.mean(fold_r2s))
+    std_r2 = float(np.std(fold_r2s))
+    fold_mapes = [] #mean ape for each fold
     for i, apes in enumerate(all_fold_apes):
         if len(apes) > 0:
             mape = np.mean(apes)
@@ -164,10 +126,16 @@ def cross_validate(
             fold_mapes.append(np.nan)
     mean_mape = np.nanmean(fold_mapes)
     print(f" MAPE of this Model: {mean_mape:.2f}%")
-  
-    mean_loss = float(np.mean(fold_losses))
-    std_loss  = float(np.std(fold_losses))
-    mean_r2 = float(np.mean(fold_r2s))
-    std_r2 = float(np.std(fold_r2s))
-    #return mean_loss, std_loss, mean_r2, std_r2
-    return mean_loss, std_loss, mean_r2, std_r2, all_fold_apes
+    #store in to golbal dictionary
+    all_the_results["all_y"] = all_y
+    all_the_results["all_preds"] = all_preds
+    all_the_results["mean_mse"] = mean_mse
+    all_the_results["std_mse"] = std_mse
+    all_the_results["mean_r2"] = mean_r2
+    all_the_results["std_r2"] = std_r2
+    all_the_results["all_fold_apes"] = all_fold_apes
+    all_the_results["fold_mapes"] = fold_mapes
+    all_the_results["mean_mape"] = mean_mape
+    all_the_results["fold_losses"] = fold_losses
+    all_the_results["fold_r2s"] = fold_r2s
+    print("All the results:", all_the_results)
