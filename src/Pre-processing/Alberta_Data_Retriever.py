80dfecacc3efed17ddd674ee1ae0fb9ebd42df3b
Saeed Shadkam <74566628+SaeedShadkam@users.noreply.github.com>
2025-05-14 14:20:18 -0600

    ...

---
+++ src/Pre-processing/Alberta_Data_Retriever.py
@@ -0,0 +1,47 @@
+#Retreiving the Alberta Trade Data
+import pandas as pd
+import numpy as np
+import pyodbc
+
+
+conn = pyodbc.connect(
+    'DRIVER={ODBC Driver 17 for SQL Server};'
+    'SERVER=c-goa-sql-10593;'
+    'DATABASE=JET_TRDAT_UAT;'
+    'Trusted_Connection=yes;'
+)
+
+Country_codes = pd.read_sql("SELECT * FROM dbo.countryCodesDescriptionStatCanUNComTradeUSCensusBureau", conn)
+# Country codes are not unique, so we need to drop duplicates
+Country_codes = Country_codes.drop_duplicates(subset=['ctyCode'])
+
+
+for year in range(2013, 2024):
+    print(year)
+    exports = pd.read_sql(f"SELECT * FROM dbo.statCanExportDataMonthly{year}", conn)
+    exports = exports.groupby(['hs6Code', 'ctyCode', 'provCode'])[['Value', 'Quantity']].sum().reset_index()
+    exports['Year'] = year
+    exports = exports.loc[: ,[ 'Year', 'hs6Code', 'ctyCode', 'provCode', 'Value', 'Quantity']]
+    exports.rename(columns={'ctyCode': 'Country', 'provCode': 'Province'}, inplace=True)
+    Alberta = exports.loc[exports['Province'] == 'AB'].reset_index(drop=True)
+    Alberta['Province'] = 'Alberta'
+    Alberta = pd.merge(Alberta, Country_codes[['UNComTradeCtyId', 'ctyCode']], how='left', left_on='Country', right_on='ctyCode')
+    Alberta.drop(columns=['Country', 'ctyCode'], inplace=True)
+    Alberta.rename(columns={'Year': 't','Province': 'i', 'UNComTradeCtyId':'j', 'hs6Code': 'k', 'Value':'v', 'Quantity':'q'}, inplace=True)
+
+    Canada = exports.groupby(['hs6Code', 'Country'])[['Value', 'Quantity']].sum().reset_index()
+    Canada['Province'] = 'Canada'
+    Canada['Year'] = year
+    Canada = pd.merge(Canada, Country_codes[['UNComTradeCtyId', 'ctyCode']], how='left', left_on='Country', right_on='ctyCode')
+    Canada.drop(columns=['Country', 'ctyCode'], inplace=True)
+    Canada.rename(columns={'Year': 't','Province': 'i', 'UNComTradeCtyId':'j', 'hs6Code': 'k', 'Value':'v', 'Quantity':'q'}, inplace=True)
+
+    
+    CEPII = pd.read_csv(f'../TradeHorizonScan/src/Pre-processing/data/CEPII/BACI_HS12_Y{year}_V202501.csv')
+    CEPII.head(1)
+
+    # Transform the Value and Quantity columns to match the CEPII calculations
+
+    Alberta = Alberta.loc[:, CEPII.columns]
+    Canada = Canada.loc[:, CEPII.columns]
+    CEPII = pd.concat([CEPII, Alberta], ignore_index=True)
--- src/alberta_diversified.py
+++ src/alberta_diversified.py
@@ -6,69 +6,6 @@ import torch
 from src.model import TradeHorizonScanModel
 
 
-#Retreiving the Alberta Trade Data
-import pyodbc
-conn = pyodbc.connect(
-    'DRIVER={ODBC Driver 17 for SQL Server};'
-    'SERVER=c-goa-sql-10593;'
-    'DATABASE=JET_TRDAT_UAT;'
-    'Trusted_Connection=yes;'
-)
-data = []
-for year in range(2013, 2025):
-    exports = pd.read_sql(f"SELECT * FROM dbo.statCanExportDataMonthly{year}", conn)
-    exports['hs4Code'] = exports['hs6Code'].astype(str).str.zfill(6).str[:4]
-    exports = exports.groupby(['hs4Code', 'ctyCode', 'provCode'])[['Value', 'Quantity']].sum().reset_index()
-    exports['Year'] = year
-    exports = exports.loc[: ,[ 'Year', 'hs4Code', 'ctyCode', 'provCode', 'Value', 'Quantity']]
-    exports.rename(columns={'ctyCode': 'Country', 'provCode': 'Province'}, inplace=True)
-    data.append(exports)
-exports = pd.concat(data, ignore_index=True)
-
-
-
-Alberta = exports[exports['Province'] == 'AB'].reset_index(drop=True)
-Alberta.drop(columns=['Province'], inplace=True)
-Alberta['AvgUnitPrice'] = Alberta['Value'] / Alberta['Quantity']
-Alberta['AvgUnitPriceFlags'] = Alberta.AvgUnitPrice == np.inf
-Alberta['AvgUnitPrice'] = Alberta['AvgUnitPrice'].replace(np.inf, 0)
-
-Canada = exports.groupby(['Year', 'hs4Code', 'Country'])[['Value', 'Quantity']].sum().reset_index()
-Canada.rename(columns={'Value': 'CA_Value', 'Quantity': 'CA_Quantity'}, inplace=True)
-Canada['CA_AvgUnitPrice'] = Canada['CA_Value'] / Canada['CA_Quantity']
-Canada['CA_AvgUnitPriceFlags'] = Canada.CA_AvgUnitPrice == np.inf
-Canada['CA_AvgUnitPrice'] = Canada['CA_AvgUnitPrice'].replace(np.inf, 0)
-
-# Merge with Ablerta data
-Alberta = Alberta.merge(Canada, on=['Year', 'hs4Code', 'Country'], how='left')
-Alberta
-
-pd.read_csv('../TradeHorizonScan/src/Pre-processing/1- CEPII_Processed_HS4_2013_2023.csv').head()
-import os
-print(os.getcwd())
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 # Loading the datasets
