110a528c7366192f5b36d64720554abeab3725d6
Saeed Shadkam <74566628+SaeedShadkam@users.noreply.github.com>
2025-05-14 14:53:33 -0600

    ...

--- src/Pre-processing/Alberta_Data_Retriever.py
+++ src/Pre-processing/Alberta_Data_Retriever.py
@@ -13,8 +13,11 @@ conn = pyodbc.connect(
 
 Country_codes = pd.read_sql("SELECT * FROM dbo.countryCodesDescriptionStatCanUNComTradeUSCensusBureau", conn)
 # Country codes are not unique, so we need to drop duplicates
-Country_codes = Country_codes.drop_duplicates(subset=['ctyCode'])
-
+Country_codes = Country_codes.loc[:, ['ctyCode', 'UNComTradeCtyId']]
+Country_codes = Country_codes.drop_duplicates(subset=['ctyCode'], keep='first')
+# Notice all differennt UNComTradeCtyId are included in the furhter processing dat
+# SO Here for each ctyCode I pick the first available UNComTradeCtyId
+# And drop the rest to prevfent double accounting
 
 for year in range(2013, 2024):
     print(year)
@@ -25,7 +28,7 @@ for year in range(2013, 2024):
     exports.rename(columns={'ctyCode': 'Country', 'provCode': 'Province'}, inplace=True)
     Alberta = exports.loc[exports['Province'] == 'AB'].reset_index(drop=True)
     Alberta['Province'] = 'Alberta'
-    Alberta = pd.merge(Alberta, Country_codes[['UNComTradeCtyId', 'ctyCode']], how='left', left_on='Country', right_on='ctyCode')
+    Alberta = pd.merge(Alberta, Country_codes, how='left', left_on='Country', right_on='ctyCode')
     Alberta.drop(columns=['Country', 'ctyCode'], inplace=True)
     Alberta.rename(columns={'Year': 't','Province': 'i', 'UNComTradeCtyId':'j', 'hs6Code': 'k', 'Value':'v', 'Quantity':'q'}, inplace=True)
 
@@ -38,9 +41,14 @@ for year in range(2013, 2024):
 
     
     CEPII = pd.read_csv(f'../TradeHorizonScan/src/Pre-processing/data/CEPII/BACI_HS12_Y{year}_V202501.csv')
-    CEPII.head(1)
-
     # Transform the Value and Quantity columns to match the CEPII calculations
+
+    # CEPII values are in thousands of dollars, so we need to divide by 1000
+    Alberta['v'] = Alberta['v'] / 1000
+    Canada['v'] = Canada['v'] / 1000    
+
+
+
 
     Alberta = Alberta.loc[:, CEPII.columns]
     Canada = Canada.loc[:, CEPII.columns]
