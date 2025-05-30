118932bbaf4cdf5503b862ab08065e4f6bbff81e
Saeed Shadkam <74566628+SaeedShadkam@users.noreply.github.com>
2025-05-30 13:56:36 -0600

    ...

--- src/Pre-processing/GTA_DATA_Download.py
+++ src/Pre-processing/GTA_DATA_Download.py
@@ -18,6 +18,59 @@ headers = {
 
 
 
+#set up payload for API request
+payload = {
+    "limit": 1000,  
+    "offset": 0,
+    "request_data": { 
+        "implementer": country_codes, #test 5 country
+        "affected": [842, 124],  # Implementor country
+        "gta_evaluation": [4, 5],  # 4: Harmful, 5: Liberalising
+        #"affected_products": [2790],
+        "affected_flow": [1, 2, 3] # 1: Inward, 2: Outward, 3: Outward subsidy
+    }
+}
+
+
+
+response = requests.post(url, headers=headers, json=payload)
+print("Status Code:", response.status_code)
+response = response.json()[0]
+response.keys()
+response['intervention_id']
+response
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if response.status_code == 200:
+    # Save the data to a file
+    with open("gta_data.json", "w", encoding="utf-8") as f:
+        f.write(response.text)
+    print("Data downloaded and saved successfully.")
+else:
+    print(f"Failed to download data. Status code: {response.status_code}")
+    print(response.text)
+
+
 #setup for country year, hscode
 country_codes = [784, 32, 36, 58, 56, 50, 124, 757, 756, 156, 170, 218, 251, 250, 826, 320, 344, 360,
                  699, 356, 380, 392, 410, 484, 458, 566, 528, 604, 608, 682, 702, 764, 158, 840, 841,
@@ -39,39 +92,7 @@ print("Extract attached HS4 codes:", HS_codes)
 
 
 
-#set up payload for API request
-payload = {
-    "limit": 1000,  
-    "offset": 0,
-    "sorting": "-date_announced",
-    "request_data": { 
-        "implementing_jurisdiction": [{"name": "China", "code": 156}], #test 5 country
-        "affected": countries_code,  # Implementor country
-        "event_period":years,  # 2013-2023 inclusive
-        "gta_evaluation": "Harmful",  # 4: Harmful, 5: Liberalising
-        "affected_products": HS_codes
-    }
-}
-
-
-response = requests.post(url, headers=headers, json=payload)
-
-
-if response.status_code == 200:
-    # Save the data to a file
-    with open("gta_data.json", "w", encoding="utf-8") as f:
-        f.write(response.text)
-    print("Data downloaded and saved successfully.")
-else:
-    print(f"Failed to download data. Status code: {response.status_code}")
-    print(response.text)
-
-
-
-
-
-
-
 
 #transfer json to pandas dataframe
 content_json = json.loads(response.text)
+content_json.keys()
