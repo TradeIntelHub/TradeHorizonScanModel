5cdfdd5940adea74089c088b7c8aa0be34d91a0e
Xinyue Fan <forlafan399@gmail.com>
2025-05-30 09:36:51 -0600

    draft for GTA download

--- src/Pre-processing/GTA_DATA_Download.py
+++ src/Pre-processing/GTA_DATA_Download.py
@@ -8,49 +8,54 @@ import pandas as pd
 #harmful 4 anf liberlising 5: gta_evaluation
 #perid: event_period
 
-
-
-
-# Example API endpoint (replace with your actual API URL)
-api_url = "b27cbe9a8908e84834f81e616ef7b4af87a7f705"
-
+api_key = "b27cbe9a8908e84834f81e616ef7b4af87a7f705"
+url = "https://api.globaltradealert.org/api/v1/data/"
 headers = {
     "Content-Type": "application/json",
-    "Authorization": "APIKey b27cbe9a8908e84834f81e616ef7b4af87a7f705"
+    "Authorization": f"APIKey {api_key}"
 }
 
+
+
+
 #setup for country year, hscode
-country_codes = [784, 32, 36, 58, 56, 50, 124, 757, 756, 156, 170, 218, 251, 250, 826, 320, 344, 360, 699, 356, 380, 392, 410, 484, 458, 566, 528, 604, 608, 682, 702, 764, 158, 840, 841, 842, 868, 704]
+country_codes = [784, 32, 36, 58, 56, 50, 124, 757, 756, 156, 170, 218, 251, 250, 826, 320, 344, 360,
+                 699, 356, 380, 392, 410, 484, 458, 566, 528, 604, 608, 682, 702, 764, 158, 840, 841,
+                 842, 868, 704]
 countries_code = [32, 156, 124, 608, 360]
 countries_name = ['Argantina', 'China', 'Canada', 'Philippines', 'Indonesia']
 
 years = [str(year) + "-01-01" for year in range(2013, 2024)]
 
 
-#GET HS4 CODE LIST
-HS4_path = "'../TradeHorizonScan/data/HS4CODE.csv'" 
-HS_codes_df = pd.read_excel(HS4_path, header=None, names=['ID'], dtype={'ID': str})
-HS_codes = HS_codes_df['ID'].tolist()
+HS4_path = "../TradeHorizonScan/data/HS4CODE.csv" 
+HS_codes_df = pd.read_csv(HS4_path, header=None, names=['ID'], dtype={'ID': str})
+HS_codes = HS_codes_df['ID'].tolist() # Convert to string 
 HS_code_digits = 4
-HS_codes = [f'{int(code):0{HS_code_digits}d}' for code in HS_codes]
+HS_codes = [f'{int(code):0{HS_code_digits}d}' for code in HS_codes] # ensure all codes are 4 digits
 print("Extract attached HS4 codes:", HS_codes)
+
+
+
+
 
 #set up payload for API request
 payload = {
-    "limit": 10000,  
+    "limit": 1000,  
     "offset": 0,
     "sorting": "-date_announced",
     "request_data": { 
-        "implementing_jurisdiction": countries_code,
-        "affected": countries_code,
+        "implementing_jurisdiction": [32], #test 5 country
+        "affected": [36],
         "event_period":years,  # 2013-2023 inclusive
         "gta_evaluation": [4, 5],  # 4: Harmful, 5: Liberalising
-        "affected_products": "HS4"
+        "affected_products": HS_codes
     }
 }
 
 
-response = requests.post(api_url, headers=headers, json=payload)
+response = requests.post(url, headers=headers, json=payload)
+
 
 if response.status_code == 200:
     # Save the data to a file
@@ -60,3 +65,13 @@ if response.status_code == 200:
 else:
     print(f"Failed to download data. Status code: {response.status_code}")
     print(response.text)
+
+
+
+
+
+
+
+
+#transfer json to pandas dataframe
+content_json = json.loads(response.text)
